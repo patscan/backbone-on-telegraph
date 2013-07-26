bb0789b8830d84ead256fb122b15cd60c35ef99f
patscan <patscan@gmail.com>
2013-07-26 00:48:34 -0700

    First commit. Boilerplate rails install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneOnTelegraph::Application.config.secret_token = '7d06a999a3058ae211fd392d6d6af833c492a8404be6202ec3b1d82322bf1cd2e2d132d7ada22bebbe0d17abd0ecd8d405c5d170ff08fdf0491b7a6dfe2c7731'
