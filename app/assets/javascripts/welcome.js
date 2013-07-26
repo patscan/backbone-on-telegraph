7b69ad7d1d0323a8431a53ac56b4fd2226412e38
patscan <patscan@gmail.com>
2013-07-26 02:20:13 -0700

    Set up basic Backbone functionality!
    From here, the world is my oyster...

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,49 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(document).ready(function(){
+  Song = Backbone.Model.extend({});
+
+  var song = new Song();
+  song.url = '/songs/1.json'
+
+  song.fetch({
+    success : function(){
+      console.log(song.get('title'));
+    } 
+  });
+
+  //-------------------------------------
+
+  Songs = Backbone.Collection.extend({
+    model : Song
+  });
+  var songs = new Songs();
+
+  songs.url = '/songs.json'
+
+  songs.fetch({
+    success : function(){
+      _.each(songs.models, function(model){
+        view = new SongView({ model : model });
+        $('.songs').append(view.render());
+      });
+      console.log(songs);
+    } 
+  });
+
+  //-------------------------------------
+
+  var SongView = Backbone.View.extend({
+    tagName : 'li',
+    events : {
+      'click' : 'alertDescription',
+    },
+    alertDescription : function() {
+      alert(this.model.get('description'))
+    },
+    render : function() {
+      return $(this.el).text(this.model.get('title'));
+    }
+  });
+
+
+
+});
